7339b057e6ae70c348cb286691e078e01a6e3f74
YoGi <yogeshmishra667@gmail.com>
2020-07-02 00:33:22 +0530

    added cart design structure

--- frontend/src/components/product/cartScreen.js
+++ frontend/src/components/product/cartScreen.js
@@ -19,5 +19,43 @@ export default function CartScreen(props) {
     };
   }, []);
 
-  return <>cart</>;
+  return (
+    <div className="cart">
+      <div className="cart-list">
+        <ul className="cart-list-container">
+          <li>
+            <h3>Shopping Cart</h3>
+            <div>Price</div>
+          </li>
+          {cartItems.length === 0 ? (
+            <div>Cart is empty</div>
+          ) : (
+            cartItems.map((item) => (
+              <li>
+                <div className="cart-image">
+                  <img src={item.image} alt="product" />
+                </div>
+                <div className="cart-name">
+                  <div>
+                    Qty:
+                    <select>
+                      <option>1</option>
+                      <option>2</option>
+                      <option>3</option>
+                    </select>
+                  </div>
+                </div>
+                <div className="cart-price">${item.price}</div>
+              </li>
+            ))
+          )}
+        </ul>
+      </div>
+      <div className="cart-action">
+        <button className="button primary full-width">
+          Proceed to Checkout
+        </button>
+      </div>
+    </div>
+  );
 }
